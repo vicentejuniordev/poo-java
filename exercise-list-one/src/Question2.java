c0c3a26f40ff761a315aa569a6e0d53234bae314
vicente <vicentejunior4522@gmail.com>
2025-08-31 17:57:25 -0300

    awnser question 2

--- exercise-list-one/src/Question2.java
+++ exercise-list-one/src/Question2.java
@@ -1,8 +1,18 @@
 import java.util.Scanner;
 
 public class Question2 {
-  public static void main(String[] args) {
+  public static void main(String[] args)  {
     Scanner scanner = new Scanner(System.in);
-    
+    float measures = 1;
+    for(int i = 0; i < 2; i++){
+      float measure = scanner.nextFloat();
+      measures *= measure;
+    }
+    scanner.close();
+
+    float area = measures/2;
+
+    System.out.println(area);
+
   }
 }
