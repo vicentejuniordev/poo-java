6732efcb58203bf409d530ca1212e2a6b9a41fb7
vicente <vicentejunior4522@gmail.com>
2025-08-31 18:19:46 -0300

    Awnser question 7

--- exercise-list-one/src/Question7.java
+++ exercise-list-one/src/Question7.java
@@ -1,3 +1,16 @@
+import java.util.Scanner;
+
 public class Question7 {
-  
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    float valPrest = scanner.nextFloat();
+    int temp = scanner.nextInt();
+    float taxa = scanner.nextFloat();
+    scanner.close();
+
+    float prest = valPrest + (valPrest + (taxa/ 100) * temp);
+
+    System.out.println(prest);
+  }
 }
