638ab5cf003c6cfb2d6b29e0333b3551a5da103c
vicenteprogrammer <vicentejunior4522@gmail.com>
2025-09-14 18:19:29 -0300

    anwnser question 11

---
+++ exercise-list-two/src/VerificaVogal.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class VerificaVogal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite uma letra: ");
+        char letra = scanner.next().toLowerCase().charAt(0); 
+
+        switch (letra) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                System.out.println("É uma vogal.");
+                break;
+            default:
+                System.out.println("Não é uma vogal.");
+        }
+
+        scanner.close();
+    }
+}
