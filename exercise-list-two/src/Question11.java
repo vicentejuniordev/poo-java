822705f2b33f1de0f0afb173a65d03fa06eadd5a
vicenteprogrammer <vicentejunior4522@gmail.com>
2025-09-14 18:20:00 -0300

    anwnser question 11

--- exercise-list-two/src/Question11.java
+++ exercise-list-two/src/Question11.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class VerificaVogal {
+public class Question11 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
