cc8a59b672c50759b970aac487e9519b63d0c080
vicenteprogrammer <vicentejunior4522@gmail.com>
2025-09-14 18:15:21 -0300

    awnser question 10

---
+++ exercise-list-two/src/Question10.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Question10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int day = scanner.nextInt();
+        switch (day) {
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Segunda-feira");
+                break;
+            case 3:
+                System.out.println("Terça-feira");
+                break;
+            case 4:
+                System.out.println("Quarta-feira");
+                break;
+            case 5:
+                System.out.println("Quinta-feira");
+                break;
+            case 6:
+                System.out.println("Sexta-feira");
+                break;
+            case 7:
+                System.out.println("Sábado");
+                break;
+            default:
+            System.out.println("Opção inválida");
+                break;
+        }
+        scanner.close();
+    }
+}
