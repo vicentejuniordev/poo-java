7f088badbe69477baa5afefc286eafdbaeadba2c
vicenteprogrammer <vicentejunior4522@gmail.com>
2025-09-14 18:23:58 -0300

    awnser question 12

---
+++ exercise-list-two/src/Question12.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Question12 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        System.out.println(number%2==0?"Número Par":"Número Ímpar");
+        scanner.close();
+    }
+}
