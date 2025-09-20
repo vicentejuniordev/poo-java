69b994c8da48734f6e3c5fda25caf8c19867384a
vicenteprogrammer <vicentejunior4522@gmail.com>
2025-09-20 18:30:43 -0300

    awnser question 1

---
+++ exercise-list-three/src/Question1.java
@@ -0,0 +1,7 @@
+public class Question1 {
+    public static void main(String[] args) throws Exception {
+        for(int i = 15; i <= 200; i++ ){
+            System.out.println(i * i);
+        }
+    }
+}
