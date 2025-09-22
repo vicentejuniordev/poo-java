e2964d395c6a9441b0adccc23bdfd8038b7e4913
vicenteprogrammer <vicentejunior4522@gmail.com>
2025-09-22 14:41:20 -0300

    awnser question 10

---
+++ exercise-list-three/src/Question10.java
@@ -0,0 +1,8 @@
+public class Question10 {
+    public static void main(String[] args) {
+        for(int i = 10; i <= 100; i+=10){
+
+            System.out.println(i+" GRAUS -> " + (i * 9/5 + 32) + " FAHRENHEIT");
+        }
+    }
+}
